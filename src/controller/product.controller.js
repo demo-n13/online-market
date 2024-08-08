0cd9537b7191f25a4ec5f2012ad0fde4cf184a04
maverizes <ahmadjoncoder06@gmail.com>
2024-08-08 12:20:05 +0500

    update and delete product created successful

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -47,3 +47,36 @@ export async function createProduct(req, res) {
     data: newProduct,
   });
 }
+
+export async function deleteProduct(req, res) {
+  const { id } = req.params;
+  const response = await fetchData("DELETE FROM products WHERE id = $1", id);
+  if (response.rowCount === 0) {
+      res.status(404).send({ message: "Product not found" });
+  } else {
+      res.send({ message: "Product deleted successfully" });
+  }
+}
+
+
+
+export async function updateProduct(req, res) {
+  const { id } = req.params;
+  const data = req.body;
+  const response = await fetchData(
+      "UPDATE products SET title = $1, description = $2, price = $3, image_url = $4, rating = $5, category_id = $6 WHERE id = $7",
+      data.title,
+      data.description,
+      data.price,
+      data.image_url,
+      data.rating,
+      data.category_id,
+      id
+  );
+  if (response.rowCount === 0) {
+      res.status(404).send({ message: "Product not found" });
+  } else {
+      res.send(response);
+  }
+}
+
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,6 +3,8 @@ import {
   createProduct,
   getProductsByCategory,
   getSingleProduct,
+  updateProduct,
+  deleteProduct,
 } from "../controller/product.controller.js";
 
 export const productRoutes = Router();
@@ -10,4 +12,6 @@ export const productRoutes = Router();
 productRoutes
   .get("/products/by/category/:categoryId", getProductsByCategory)
   .get("/products/:productId", getSingleProduct)
-  .post("/products/add", createProduct);
+  .post("/products/add", createProduct)
+  .put("/put", updateProduct)
+  .delete("/delete", deleteProduct)
