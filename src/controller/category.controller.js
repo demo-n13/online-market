6a40ceccb968696e3dbc9ddb6e710cd815e05577
Yuldoshova <ozoda8171@gmail.com>
2024-08-08 12:16:59 +0500

    added update method

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -12,7 +12,7 @@ export async function getAllCategory(req, res) {
       c.id
     );
 
-    for(const sb of subCategories) {
+    for (const sb of subCategories) {
       const allProducts = await fetchData("SELECT * FROM product WHERE category_id = $1", sb.id)
 
       sb.products = allProducts
@@ -39,4 +39,41 @@ export async function createCategory(req, res) {
   );
 
   res.send(response);
+}
+
+export async function updateCategory(req, res) {
+  const id = req.params.categoryId;
+  const data = req.body;
+
+  const category = await fetchData(`SELECT * FROM category WHERE id=${id}`)
+  if (category.length == 0) {
+    res.send({
+      status: 404,
+      message: "Category not found!",
+    });
+    return;
+  }
+
+  if (!data.name || !data.image_url) {
+    res.send({
+      status: 400,
+      message: "Bad request!"
+    })
+    return;
+  }
+
+  const updateCate = await fetchData(
+    "UPDATE category SET name=$1, image_url=$2, category_id=$3 WHERE id=$4 RETURNING *",
+    data.name,
+    data.image_url,
+    data.category_id,
+    id
+  )
+
+  res.send({
+    status: 204,
+    message: "Successfully updated!",
+    data: updateCate,
+  });
+  return;
 }
